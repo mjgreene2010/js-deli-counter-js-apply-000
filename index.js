768b78689f3fd5f86f55c6428500fe8ae171e529
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-24 15:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function takeANumber(line, name){
 }
 
 function takeANumberAlternate(line){
-  var x = 1 
-  line.push(++x)
+  var x = 0
+  x++
+  line.push(x)
   return `Welcome you are number ${x}.`
 }
 
